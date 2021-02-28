297449ec3715bdc7f080456714c76200015c67a0
張佑崧 <31153913+Yorkchung@users.noreply.github.com>
2021-02-28 20:42:05 +0800

    Create credentials.h

---
+++ arduino/credentials.h
@@ -0,0 +1,7 @@
+char networkSSID[] = /*"your wifi id"*/;
+char networkPASSWORD[] = /*"your wifi password"*/;
+
+char mqttSERVER[] = /*"your pc ip"*/;
+char mqttUSERNAME[] = /*"mqtt name"*/;
+char thingID[] = "YOUR_THING_ID";
+char thingKEY[] = /*"mqtt password"*/;
